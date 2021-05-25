6d21ca884cce4d49b75f7a45307684958e4eee42
Zubair Beg <ziibii88@gmail.com>
2021-05-25 13:45:36 +0800

    added userinput collection and clearing

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,12 @@
+// AutoBind decorator (target and methodName parameters unused)
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    return {
+        configurable: true,
+        get() { return descriptor.value.bind(this); }
+    } as PropertyDescriptor
+}
+
+// ProjectInput class
 class ProjectInput {
     templateElem: HTMLTemplateElement;
     hostElem: HTMLDivElement;
@@ -23,14 +32,35 @@ class ProjectInput {
         this.attach();
     }
 
-    private submitHandler(event: Event) {
-        event.preventDefault();
-        console.log(this.titleInputEl.value);
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputEl.value.trim();
+        const enteredDesc = this.descInputEl.value.trim();
+        const enteredPeople = this.peopleInputEl.value.trim();
+
+        // empty or blank validation
+        if (enteredTitle.length === 0 || enteredDesc.length === 0 || enteredPeople.length === 0) {
+            alert("Invalid input: missing fields! Please try again!");
+        } else {
+            return [enteredTitle, enteredDesc, +enteredPeople];
+        }
     }
 
-    private configure() { this.formElem.addEventListener('submit', this.submitHandler.bind(this))}
+    private clearInput() {
+        this.titleInputEl.value = ''
+        this.descInputEl.value = ''
+        this.peopleInputEl.value = ''
+    }
+
+    @autobind // bind 'this' properly to method
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) { console.log(userInput); this.clearInput(); }
+    }
+
+    private configure() { this.formElem.addEventListener('submit', this.submitHandler)}
 
     private attach() { this.hostElem.insertAdjacentElement('afterbegin', this.formElem); }
 }
 
-const projInput = new ProjectInput();
+new ProjectInput();
